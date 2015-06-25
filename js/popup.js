07800ee923c075bd68e15843689d487e639bf6a5
AlphaHinex <AlphaHinex@gmail.com>
2015-06-25 21:44:19 +0800

    Save settings in localStorage

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,29 @@
+var settings = {
+  get: function(key) {
+    return localStorage.getItem(key);
+  },
+
+  put: function(key, value) {
+    localStorage.setItem(key, value);
+  }
+};
+
 $(function() {
-  var explore = $("#explore");
-  explore.attr('checked', 'true');
+  $('#explore, #stars, #profile, #network').each(function() {
+    var ele = $(this);
+    ele.attr('checked', settings.get(this.id) !== 'false');
+    ele.click(function() {
+      console.log(this.id + ':' + this.checked);
+      chrome.runtime.sendMessage({settingChanged: this.id + ':' + this.checked});
+    });
+  });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.hasOwnProperty('settingChanged')) {
+      var kv = request['settingChanged'].split(':');
+      settings.put(kv[0], kv[1]);
+    } 
+  }
+);
