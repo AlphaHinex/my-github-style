f27b017729d85958d54b92a1ea4eb24e6da22122
AlphaHinex <AlphaHinex@gmail.com>
2015-07-25 12:49:13 +0800

    Add select switch

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,32 @@
 $(function() {
-  $('#explore, #stars, #profile, #gototop, #network, #gist, #fixheader').each(function() {
+  chrome.storage.sync.get('switch', function(val) {
+    $('#switch').attr('checked', val['switch'] !== 'false');
+  });
+
+  $('#switch').click(function() {
+    var sw = this;
+    storeState(sw);
+
+    $('.feat').each(function() {
+      this.checked = sw.checked;
+      storeState(this);
+    });
+  });
+
+  $('.feat').each(function() {
     var ele = $(this),
         id = this.id;
     ele.click(function() {
-      chrome.runtime.sendMessage({settingChanged: this.id + ':' + this.checked});
+      storeState(this);
     });
     chrome.storage.sync.get(this.id, function(val) {
       ele.attr('checked', val[id] !== 'false');
     });
   });
+
+  var storeState = function(feat) {
+    chrome.runtime.sendMessage({settingChanged: feat.id + ':' + feat.checked});
+  };
 });
 
 chrome.runtime.onMessage.addListener(
